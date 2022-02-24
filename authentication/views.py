6f07e1c434e693b501be3ceb45463929333b77b5
swathijs1619 <swathijs.181co155@nitk.edu.in>
2022-02-24 20:00:39 +0530

    Added templates

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,23 @@
+from base64 import urlsafe_b64decode
+from email import message
+import imp
+from lib2to3.pgen2.tokenize import generate_tokens
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from gfg import settings
+from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode
+from django.core.mail import send_mail, EmailMessage
+
+from gfg.info import EMAIL_HOST_USER
+from . tokens import generate_token
 
 # Create your views here.
 def home(request):
@@ -19,23 +34,85 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
 
-        myuser = User.objects.create(username, email, pass1)
+        if User.objects.filter(username=username):
+            message.error(request, "Username already exists! Please write some other username")
+            return redirect('home')
+        
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already registered!")
+            return redirect('home')
+        
+        if len(username) > 10:
+            messages.error(request, "Username must not exceed 10 characters!")
+            return redirect('home')
+        
+        if pass1 != pass2:
+            messages.error(request, "Passwords didn't match!")
+            return redirect('home')
+        
+        if not username.isalnum():
+            messages.error(request, "Username must be Alpha-numeric!")
+            return redirect('home')
+
+        myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
-
+        myuser.is_active = False
         myuser.save()
 
-        messages.success(request, "Your account has been successfully created.")
+        messages.success(request, "Your account has been successfully created. We have sent you a confirmation email, please confirm your email inorder to activate your account.")
 
+        # Welcome Email
+
+        subject = "Welcome to GFG Django login"
+        message = "Hello " + myuser.first_name + "!! \n" + "Welcome to GFG!! \n Thank you for visiting our website. Please click on the below sent link for the confirmation of your account"
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
+
+
+        # Email Address Confirmation
+
+        current_site = get_current_site(request)
+        email_subject = "Confirm your email @GFG - Django Login!!"
+        message2 = render_to_string('email_confirmation.html', {
+            'name': myuser.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser)
+            }
+            email = EmailMessage(
+                email_subject,
+                message2, 
+                settings.EMAIL_HOST_USER,
+                [myuser.email],
+            )
+            email.fail_silently = True
+            email.send())
+      
         return redirect('signin')
-
-
-
-
+   
     return render(request, "authentication/signup.html")
 
 def signin(request):
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        pass1 = request.POST["pass1"]
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "authentication/index.html", {'fname': fname})
+        else:
+            messages.error(request, "Bad Credentials!")
+            return redirect('home')
+
     return render(request, "authentication/signin.html")
 
 def signout(request):
-    pass
+    logout(request)
+    messages.success(request, "Logged Out Successfully!")
+    return redirect('home')
---
+++ gfg/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'userauthenticationsystemnitk@gmail.com'
+EMAIL_HOST_PASSWORD = 'Nitk@2022'
+EMAIL_PORT = 587
