6ebb80c37e9ae30ddf61e065ace4ab9f6200c2b2
swathijs1619 <swathijs.181co155@nitk.edu.in>
2022-02-24 15:33:56 +0530

    Added Django APP

---
+++ authentication/views.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+# Create your views here.
+def home(request):
+    return render(request, 'authentication/index.html')
+
+def signup(request):
+
+    if request.method == "POST":
+        #username = request.POST.get('username')
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created.")
+
+        return redirect('signin')
+
+
+
+
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    pass
