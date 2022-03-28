9b8379cb8f5593c69b3776f55d2982706f7973b4
swathijs1619 <swathijs.181co155@nitk.edu.in>
2022-03-28 22:40:41 +0530

    Password Change done

--- Login-Logout-PasswordChange-and-PasswordReset/MyProject/MyProject/settings.py
+++ Login-Logout-PasswordChange-and-PasswordReset/MyProject/MyProject/settings.py
@@ -126,3 +126,16 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if not DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST_USER = 'userauthenticationsystemnitk@gmail.com'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_PASSWORD = "Nitk@2022"
+
+else:
+    EMAIL_BACKEND = (
+        "django.core.mail.backends.console.EmailBackend"
+    ) 
